7fa848ed5750ebfc1442e40e7180c58908d5c428
Onkarjot Singh <onkarjot.singh@msbdocs.com>
2023-03-27 11:21:50 +0530

    first commit

---
+++ src/main/java/com/MavenCentralApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenCentralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenCentralApplication.class, args);
+	}
+
+}
